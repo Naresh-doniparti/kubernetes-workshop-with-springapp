fe8368720cb97164c053f9a6afc15f42ac624702
Naresh Doniparti <naresh.doniparti@gmail.com>
2021-02-15 16:46:28 +0530

    v1 of the application

---
+++ src/main/java/com/kubernetes/springapp/KubernetesWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubernetes/springapp/KubernetesWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
